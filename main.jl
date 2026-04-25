71f1156997b53e251add8d157427584e117ac2bb
David Anthoff <anthoff@berkeley.edu>
2026-04-25 12:06:52 +0200

    Add some diagnostic output

--- main.jl
+++ main.jl
@@ -73,10 +73,10 @@ if !isempty(results_path)
     end
 end
 
-@debug "Before kill_test_processes()"
+@info "Before kill_test_processes()"
 
 kill_test_processes()
 
-@debug "Before final exit call"
+@info "Before final exit call"
 
 exit(at_least_one_fail ? 1 : 0)
